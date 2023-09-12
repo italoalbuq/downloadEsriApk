34b051600567f0e5b51e7b1eb5b4a8b30eb5b8c4
Italo Giullian <33552128+italoalbuq@users.noreply.github.com>
2023-09-12 15:27:36 -0300

    Update downloadEsriApk.py

--- downloadEsriApk.py
+++ downloadEsriApk.py
@@ -1,34 +1,37 @@
+import streamlit as st
 import requests
 
+# Título da aplicação
+st.title("Download de Aplicativos Esri")
+
 # nome do APP
-nome_aplicativo = input("Digite o nome do aplicativo: ")
+nome_aplicativo = st.text_input("Digite o nome do aplicativo: ")
 
 # versao_realize
-versao = input("Digite a versão desejada: ")
+versao = st.text_input("Digite a versão desejada: ")
 
-# nome do arquivo
-nome_arquivo_local = f"{nome_aplicativo.replace(' ', '_')}_{versao}.exe"
+# Botão para fazer o download
+if st.button("Baixar Aplicativo"):
+    # Inicializa a URL como vazia
+    url = ""
 
-# Inicializa a URL como vazia
-url = ""
+    # Verifica o nome do aplicativo e constrói a URL apropriada
+    if nome_aplicativo.lower() == "fieldmaps":
+        url = f"https://gisupdates.esri.com/{nome_aplicativo}/ArcGIS_Field_Maps_Android_{versao}.exe"
+    elif nome_aplicativo.lower() == "survey123":
+        url = f"https://gisupdates.esri.com/{nome_aplicativo}/3.17/ArcGIS_Survey123_Android_ARMv8_{versao}.exe"
+    else:
+        st.error("Nome de aplicativo não reconhecido. Por favor, digite 'fieldmaps' ou 'survey123'.")
 
-# Verifica o nome do aplicativo e constrói a URL apropriada
-if nome_aplicativo.lower() == "fieldmaps":
-    url = f"https://gisupdates.esri.com/{nome_aplicativo}/ArcGIS_Field_Maps_Android_{versao}.exe"
-elif nome_aplicativo.lower() == "survey123":
-    url = f"https://gisupdates.esri.com/{nome_aplicativo}/3.17/ArcGIS_Survey123_Android_ARMv8_{versao}.exe"
-else:
-    print("Nome de aplicativo não reconhecido. Por favor, digite 'fieldmaps' ou 'survey123'.")
-
-
-# Faz a solicitação GET para o URL
-response = requests.get(url)
-
-# Verifica se a solicitação foi bem-sucedida (código de status 200)
-if response.status_code == 200:
-    # Abre o arquivo local em modo binário e escreve o conteúdo da resposta nele
-    with open(nome_arquivo_local, 'wb') as arquivo_local:
-        arquivo_local.write(response.content)
-    print(f"Download concluído: {nome_arquivo_local}")
-else:
-    print(f"Erro ao fazer o download: Código de status {response.status_code}")
+    # Se a URL foi definida, faz a solicitação GET
+    if url:
+        response = requests.get(url)
+        # Verifica se a solicitação foi bem-sucedida (código de status 200)
+        if response.status_code == 200:
+            # Abre o arquivo local em modo binário e escreve o conteúdo da resposta nele
+            nome_arquivo_local = f"{nome_aplicativo.replace(' ', '_')}_{versao}.exe"
+            with open(nome_arquivo_local, 'wb') as arquivo_local:
+                arquivo_local.write(response.content)
+            st.success(f"Download concluído: {nome_arquivo_local}")
+        else:
+            st.error(f"Erro ao fazer o download: Código de status {response.status_code}")
